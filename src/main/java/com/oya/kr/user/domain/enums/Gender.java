2c088dcd920f32924e12ba91207209bcd5c0766d
sangminee <lsmlee99@gmail.com>
2024-02-14 12:53:04 +0900

    [FIX] 오류 수정

--- src/main/java/com/oya/kr/user/domain/enums/Gender.java
+++ src/main/java/com/oya/kr/user/domain/enums/Gender.java
@@ -2,11 +2,7 @@ package com.oya.kr.user.domain.enums;
 
 import static com.oya.kr.global.exception.GlobalErrorCodeList.*;
 
-import org.apache.ibatis.type.MappedTypes;
-
 import com.oya.kr.global.exception.ApplicationException;
-import com.oya.kr.global.util.CodeEnum;
-import com.oya.kr.global.util.CodeEnumTypeHandler;
 
 public enum Gender  {
 
