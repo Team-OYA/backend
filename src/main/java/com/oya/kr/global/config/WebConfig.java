7be9e77ee932ed4c222414bff801bc9a61993127
yukong <kyb062666@gmail.com>
2024-02-14 17:36:38 +0900

    [REFACTOR] 응답 형식 json 으로 설정

--- src/main/java/com/oya/kr/global/config/WebConfig.java
+++ src/main/java/com/oya/kr/global/config/WebConfig.java
@@ -1,8 +1,20 @@
 package com.oya.kr.global.config;
 
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+@Configuration
+@EnableWebMvc
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer
+	implements WebMvcConfigurer {
+
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class[] {RootConfig.class};
@@ -16,5 +28,15 @@ public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitiali
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] {"/"};
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(mappingJackson2HttpMessageConverter());
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		return new MappingJackson2HttpMessageConverter();
 	}
 }
