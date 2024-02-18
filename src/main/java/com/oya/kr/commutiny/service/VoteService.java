46c085b6de3d8064f78d793ac806e99fed22d1a1
sangminee <lsmlee99@gmail.com>
2024-02-19 01:06:10 +0900

    [FEAT] 투표 체크, 체크 취소 API 구현

---
+++ src/main/java/com/oya/kr/commutiny/service/VoteService.java
@@ -0,0 +1,53 @@
+package com.oya.kr.commutiny.service;
+
+import static com.oya.kr.commutiny.exception.CommunityErrorCodeList.*;
+
+import org.springframework.security.core.parameters.P;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.oya.kr.commutiny.mapper.VoteMapper;
+import com.oya.kr.commutiny.mapper.dto.request.VoteCheckMapperRequest;
+import com.oya.kr.global.exception.ApplicationException;
+import com.oya.kr.user.domain.User;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author 이상민
+ * @since 2024.02.18
+ */
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class VoteService {
+
+	private final VoteMapper voteMapper;
+
+	/**
+	 * 투표 체크 - 중복 투표?
+	 *
+	 * @param user, votedId
+	 * @author 이상민
+	 * @since 2024.02.18
+	 */
+	public void check(User user, long votedId) {
+		VoteCheckMapperRequest request = new VoteCheckMapperRequest(user.getId(), votedId);
+		int count = voteMapper.findById(request);
+		if(count == 1){
+			throw new ApplicationException(VALID_VOTE);
+		}
+		voteMapper.checkVote(request);
+	}
+
+	/**
+	 * 투표 체크 취소
+	 *
+	 * @param user, votedId
+	 * @author 이상민
+	 * @since 2024.02.18
+	 */
+	public void checkDelete(User user, long votedId) {
+		voteMapper.checkVoteDeleted(new VoteCheckMapperRequest(user.getId(), votedId));
+	}
+}
---
+++ src/test/java/com/oya/kr/commutiny/mapper/VoteMapperTest.java
@@ -0,0 +1,37 @@
+package com.oya.kr.commutiny.mapper;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.oya.kr.common.SpringApplicationTest;
+import com.oya.kr.commutiny.mapper.dto.request.VoteCheckMapperRequest;
+
+/**
+ * @author 이상민
+ * @since 2024.02.18
+ */
+class VoteMapperTest extends SpringApplicationTest {
+
+	@Autowired
+	private VoteMapper voteMapper;
+
+	@DisplayName("투표할 수 있다.")
+	@Test
+	void checkVote(){
+		// given
+		VoteCheckMapperRequest request = new VoteCheckMapperRequest(2, 6);
+		// when
+		voteMapper.checkVote(request);
+	}
+
+	@DisplayName("투표를 취소할 수 있다.")
+	@Test
+	void checkVoteDeleted(){
+		// given
+		VoteCheckMapperRequest request = new VoteCheckMapperRequest(2, 6);
+		// when
+		voteMapper.checkVoteDeleted(request);
+	}
+
+}
