c945580f1e650f9530ba86499e5b84360a856deb
sangminee <lsmlee99@gmail.com>
2024-02-26 16:18:57 +0900

    [CHORE] 커뮤니티 스크랩 글 불러올 때 userType null 로 변경

--- src/main/java/com/oya/kr/community/repository/CommunityRepository.java
+++ src/main/java/com/oya/kr/community/repository/CommunityRepository.java
@@ -112,7 +112,7 @@ public class CommunityRepository {
      * @since 2024.02.21
      */
     public List<CommunityBasicMapperResponse> findAllCollections(Long userId, int pageNo, int amount) {
-        return collectionMapper.findByAll(new ReadCollectionsMapperRequest(userId,false,false, "All", pageNo, amount));
+        return collectionMapper.findByAll(new ReadCollectionsMapperRequest(userId,false,false, null, pageNo, amount));
     }
 
     /**
